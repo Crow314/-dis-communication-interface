e7aa79e6b3f81e2255f548e3b88d64099e64b768
Crow314 <master@crow31415.net>
2022-01-13 15:23:59 +0900

    Evaluee packet encoding

--- app/models/evacuee.rb
+++ app/models/evacuee.rb
@@ -26,6 +26,10 @@ class Evacuee < ApplicationRecord
     (self.birth_date - MIN_BIRTH_DATE).to_i
   end
 
+  def packet
+    ([self.shelter.index, encoded_birth_date % 256, encoded_birth_date / 256] + encoded_first_name.ljust(6, "\0").bytes + encoded_family_name.ljust(6, "\0").bytes + [0]).pack("C*")
+  end
+
   private
 
   # validation
@@ -36,5 +40,18 @@ class Evacuee < ApplicationRecord
 
   def check_birth_date_be_in_range
     errors.add(:birth_date, ': 1900/01/01～2079/06/06の間で指定してください') unless MIN_BIRTH_DATE <= self.birth_date && self.birth_date <= MAX_BIRTH_DATE
+  end
+
+  class << self
+    def new_from_packet(packet)
+      binary = packet.unpack("C*")
+
+      Evacuee.new(
+        shelter: Shelter.find_by(index: binary[0]),
+        birth_date: MIN_BIRTH_DATE.days_since(binary[1]*256 + binary[2]),
+        first_name: NKF.nkf('-w -S --fb-subchar=0x3013', binary[3..8].collect {|c| c != 0 ? c.chr : '' }.join),
+        family_name: NKF.nkf('-w -S --fb-subchar=0x3013', binary[9..14].collect {|c| c != 0 ? c.chr : '' }.join)
+      )
+    end
   end
 end
