ac7c4f4b6cf999151368cd7e6cb93346860e1ed1
Crow314 <master@crow31415.net>
2022-01-13 17:01:24 +0900

    Update shelter seed

--- db/fixtures/001_shelters.rb
+++ db/fixtures/001_shelters.rb
@@ -17,10 +17,388 @@ Shelter.seed do |s|
 end
 
 Shelter.seed do |s|
+  s.id = '97d90289-7f12-4c14-b93f-b17e952809a1'
+
+  s.index = 3
+  s.name = '耳成西小学校体育館'
+  s.address = '上品寺町455-1'
+  s.phone = '0744-22-6567'
+end
+
+Shelter.seed do |s|
+  s.id = '3534b322-2b98-46a0-9a20-b3ed7a0316a3'
+
+  s.index = 4
+  s.name = '八木中学校体育館'
+  s.address = '新賀町33'
+  s.phone = '0744-22-5184'
+end
+
+Shelter.seed do |s|
+  s.id = '9e9549f3-85d9-4583-bba7-225300f487e5'
+
+  s.index = 5
+  s.name = '耳成地区公民館'
+  s.address = '葛本町593'
+  s.phone = '0744-47-3820'
+end
+
+Shelter.seed do |s|
+  s.id = '9d5789a6-7973-4c17-883f-62a556f921db'
+
+  s.index = 6
+  s.name = '福祉センター「やわらぎの郷」'
+  s.address = '十市町63-1'
+  s.phone = '0744-29-0700'
+end
+
+Shelter.seed do |s|
+  s.id = 'f4b80bc4-2093-47ff-9a43-98c07e1375fd'
+
+  s.index = 7
+  s.name = 'ひがしたけだドーム'
+  s.address = '東竹田町124-1'
+  s.phone = '0744-22-6011'
+end
+
+Shelter.seed do |s|
+  s.id = '1a47ad3c-2f5b-434e-a08d-6cf2bc8fc606'
+
+  s.index = 8
+  s.name = '橿原中学校体育館'
+  s.address = '西新堂町26-1'
+  s.phone = '0744-22-8895'
+end
+
+Shelter.seed do |s|
+  s.id = '12cc2294-be24-40b1-bee7-5d55c21ea120'
+
+  s.index = 9
+  s.name = '多地区公民館'
+  s.address = '新口町244-2'
+  s.phone = '0744-29-5835'
+end
+
+Shelter.seed do |s|
+  s.id = '1af10b1c-79d3-4131-830c-dd0777bc3025'
+
+  s.index = 10
+  s.name = '晩成小学校体育館'
+  s.address = '小房町1-54'
+  s.phone = '0744-22-6121'
+end
+
+Shelter.seed do |s|
+  s.id = '837134e8-4453-4593-905e-d445ab2f36a8'
+
+  s.index = 11
+  s.name = '八木地区公民館'
+  s.address = '北八木町2-1-6'
+  s.phone = '0744-23-6943'
+end
+
+Shelter.seed do |s|
+  s.id = 'af0a493e-fc3b-4343-bee4-762a23fc2917'
+
+  s.index = 12
+  s.name = '市立中央体育館'
+  s.address = '小房町11-1'
+  s.phone = '0744-20-1099'
+end
+
+Shelter.seed do |s|
+  s.id = '09b4bd00-6767-4c52-b3c4-4d9e54d068f1'
+
+  s.index = 13
+  s.name = '中央公民館'
+  s.address = '小房町9-23'
+  s.phone = '0744-22-2001'
+end
+
+Shelter.seed do |s|
+  s.id = '1f46585c-9c48-441d-9117-20a051c586ac'
+
+  s.index = 14
+  s.name = '奈良県立畝傍高等学校体育館'
+  s.address = '八木町3-13-2'
+  s.phone = '0744-22-5321'
+end
+
+Shelter.seed do |s|
+  s.id = '886efa25-5792-4d10-a07c-f39c6bb92830'
+
+  s.index = 15
+  s.name = '鴨公小学校体育館'
+  s.address = '縄手町324'
+  s.phone = '0744-22-2364'
+end
+
+Shelter.seed do |s|
+  s.id = 'd2183419-8060-494a-a3c4-0ff509fa2aba'
+
+  s.index = 16
+  s.name = '鴨公地区公民館'
+  s.address = '醍醐町42-2'
+  s.phone = '0744-23-5535'
+end
+
+Shelter.seed do |s|
+  s.id = '3234ec4d-035e-4dbf-8764-0bf7c1b27b2d'
+
+  s.index = 17
+  s.name = '飛騨コミュニティセンター'
+  s.address = '飛騨町94-2'
+  s.phone = '0744-22-6172'
+end
+
+Shelter.seed do |s|
+  s.id = '31af723a-6852-4039-b252-9399ffaafa74'
+
+  s.index = 18
+  s.name = '香久山小学校体育館'
+  s.address = '膳夫町98-1'
+  s.phone = '0744-22-3107'
+end
+
+Shelter.seed do |s|
+  s.id = '4cc44e57-0c61-4468-8a01-77d561d586c3'
+
+  s.index = 19
+  s.name = '香久山地区公民館'
+  s.address = '膳夫町477'
+  s.phone = '0744-22-9342'
+end
+
+Shelter.seed do |s|
+  s.id = '59957596-b4d8-4736-ad93-3634af1356f8'
+
+  s.index = 20
+  s.name = '香久山体育館'
+  s.address = '南山町624'
+  s.phone = '0744-24-2670'
+end
+
+Shelter.seed do |s|
+  s.id = 'f549a139-7583-4043-8758-6c39656a123f'
+
+  s.index = 21
+  s.name = '畝傍南小学校体育館'
+  s.address = '見瀬町164'
+  s.phone = '0744-27-1507'
+end
+
+Shelter.seed do |s|
+  s.id = '633b2835-f644-497e-87af-3008fda47c90'
+
+  s.index = 22
+  s.name = '畝傍東小学校体育館'
+  s.address = '大軽町283'
+  s.phone = '0744-27-6729'
+end
+
+Shelter.seed do |s|
+  s.id = '4192936f-1e2d-455c-b5c9-e9da78c1d88c'
+
+  s.index = 23
+  s.name = '畝傍北小学校体育館'
+  s.address = '大久保町156'
+  s.phone = '0744-22-2431'
+end
+
+Shelter.seed do |s|
+  s.id = '48946ca8-f8d7-4d4f-a441-c2268ac49c5d'
+
+  s.index = 24
+  s.name = '畝傍中学校体育館'
+  s.address = '石川町204'
+  s.phone = '0744-27-1444'
+end
+
+Shelter.seed do |s|
+  s.id = '29122a61-02cb-43da-9fbc-52143c92c8ad'
+
+  s.index = 25
+  s.name = '畝傍地区公民館'
+  s.address = '見瀬町202-1'
+  s.phone = '0744-27-7103'
+end
+
+Shelter.seed do |s|
+  s.id = '3dc76c11-7cfb-41dc-b517-7437105962a3'
+
+  s.index = 26
+  s.name = '大久保コミュニティセンター'
+  s.address = '大久保町15-1'
+  s.phone = '0744-22-1719'
+end
+
+Shelter.seed do |s|
+  s.id = '321c41ba-ac91-4211-a377-3198097e704c'
+
+  s.index = 27
+  s.name = '白橿南小学校体育館'
+  s.address = '白橿町8-19-1'
+  s.phone = '0744-27-5488'
+end
+
+Shelter.seed do |s|
+  s.id = '87d43057-d473-4812-b4eb-3c2d925d47e9'
+
+  s.index = 28
+  s.name = '白橿北小学校体育館'
+  s.address = '白橿町3-10-2'
+  s.phone = '0744-27-5900'
+end
+
+Shelter.seed do |s|
+  s.id = 'b339dd57-5543-47d1-8935-9c2af20b5247'
+
+  s.index = 29
+  s.name = '白橿中学校体育館'
+  s.address = '白橿町8-12-1'
+  s.phone = '0744-27-4883'
+end
+
+Shelter.seed do |s|
+  s.id = '34619412-8632-4999-b375-297b122de99c'
+
+  s.index = 30
+  s.name = '白橿地区公民館'
+  s.address = '白橿町2-32-11'
+  s.phone = '0744-27-2186'
+end
+
+Shelter.seed do |s|
   s.id = '28dbaffc-38cb-486c-9ff2-3f316973dc82'
 
   s.index = 31
   s.name = '今井小学校体育館'
-  s.address = '今井町3-4-46 '
+  s.address = '今井町3-4-46'
   s.phone = '0744-22-2264'
+end
+
+Shelter.seed do |s|
+  s.id = 'b896b15c-d663-47e2-a12d-0be8d4c3f3cd'
+
+  s.index = 32
+  s.name = '大成中学校体育館'
+  s.address = '小綱町77-2'
+  s.phone = '0744-22-6391'
+end
+
+Shelter.seed do |s|
+  s.id = 'b19c06db-f3f3-439d-8c6c-4c0c1860b7a5'
+
+  s.index = 33
+  s.name = '今井地区公民館'
+  s.address = '今井町2-3-23'
+  s.phone = '0744-25-3102'
+end
+
+Shelter.seed do |s|
+  s.id = 'f6c08ab3-d44b-4e02-b6d5-30a4859e302b'
+
+  s.index = 34
+  s.name = '真菅小学校体育館'
+  s.address = '曽我町710'
+  s.phone = '0744-22-6300'
+end
+
+Shelter.seed do |s|
+  s.id = 'c7c7b2ab-8994-4af5-b840-cfcc9499f2c3'
+
+  s.index = 35
+  s.name = '真菅北小学校体育館'
+  s.address = '大垣町220-1'
+  s.phone = '0744-23-6470'
+end
+
+Shelter.seed do |s|
+  s.id = '4c3cf619-3dfa-4f34-8c7f-8a49610272d2'
+
+  s.index = 36
+  s.name = '真菅地区公民館'
+  s.address = '曽我町1281-18'
+  s.phone = '0744-29-9123'
+end
+
+Shelter.seed do |s|
+  s.id = '5f35b07e-99fb-422e-b155-b8b0fc444c44'
+
+  s.index = 37
+  s.name = '曽我川緑地体育館'
+  s.address = '曽我町1212'
+  s.phone = '0744-25-2511'
+end
+
+Shelter.seed do |s|
+  s.id = '219a4cd7-5c0d-4649-a41c-63fdb7c301fd'
+
+  s.index = 38
+  s.name = '金橋小学校体育館'
+  s.address = '雲悌町216-1'
+  s.phone = '0744-22-6400'
+end
+
+Shelter.seed do |s|
+  s.id = '8bcfc8da-1ab0-44ca-a1ce-17a29562c469'
+
+  s.index = 39
+  s.name = '光陽中学校体育館'
+  s.address = '古川町20'
+  s.phone = '0744-27-1241'
+end
+
+Shelter.seed do |s|
+  s.id = 'cb2de634-accd-4e0d-9289-08b5ad52e05f'
+
+  s.index = 40
+  s.name = '金橋地区公民館'
+  s.address = '雲梯町927'
+  s.phone = '0744-23-7206'
+end
+
+Shelter.seed do |s|
+  s.id = '1ca4d36d-d707-4bba-b294-5be970b58e00'
+
+  s.index = 41
+  s.name = '橿原運動公園貴賓室'
+  s.address = '雲梯町323-2外'
+  s.phone = '0744-22-6665'
+end
+
+Shelter.seed do |s|
+  s.id = '61fb57a7-471a-4527-88cc-6dac6b102eb6'
+
+  s.index = 42
+  s.name = '新沢小学校体育館'
+  s.address = '一町1296'
+  s.phone = '0744-27-4163'
+end
+
+Shelter.seed do |s|
+  s.id = 'ebbd2905-87f9-4b4f-9af7-88e351567fc9'
+
+  s.index = 43
+  s.name = '新沢地区公民館'
+  s.address = '川西町611'
+  s.phone = '0744-28-2429'
+end
+
+Shelter.seed do |s|
+  s.id = '20295823-be58-44a9-9d56-44447c506a91'
+
+  s.index = 44
+  s.name = '橿原市新沢千塚公園拠点施設'
+  s.address = '川西町855-1'
+  s.phone = '0744-26-6201'
+end
+
+Shelter.seed do |s|
+  s.id = '3bd22499-099e-44ce-a79c-7e9f30fca7ed'
+
+  s.index = 45
+  s.name = '奈良県立橿原高等学校体育館'
+  s.address = '北越智町282'
+  s.phone = '0744-27-8282'
 end
