91edeb74a9290a4bb39830edc4b40812eb568430
Crow314 <master@crow31415.net>
2022-01-13 16:17:42 +0900

    Change Evaluee validation of name length

--- app/models/evacuee.rb
+++ app/models/evacuee.rb
@@ -7,10 +7,12 @@ class Evacuee < ApplicationRecord
 
   belongs_to :shelter
 
-  validates :first_name,  presence: true, format: { with: /\A[A-Zァ-ロワヲ-ヴー]{1,6}\z/ }
-  validates :family_name, presence: true, format: { with: /\A[A-Zァ-ロワヲ-ヴー]{1,6}\z/ }
+  validates :first_name,  presence: true, format: { with: /\A[A-Zァ-ロワヲ-ヴー]+\z/ }
+  validates :family_name, presence: true, format: { with: /\A[A-Zァ-ロワヲ-ヴー]+\z/ }
   validates :birth_date,  presence: true
 
+  validate :check_first_name_length_be_in_range
+  validate :check_family_name_length_be_in_range
   validate :check_birth_date_be_past
   validate :check_birth_date_be_in_range
 
@@ -27,12 +29,20 @@ class Evacuee < ApplicationRecord
   end
 
   def packet
-    ([self.shelter.index, encoded_birth_date % 256, encoded_birth_date / 256] + encoded_first_name.ljust(6, "\0").bytes + encoded_family_name.ljust(6, "\0").bytes + [0]).pack("C*")
+    ([self.shelter.index, encoded_birth_date % 256, encoded_birth_date / 256] + encoded_first_name.ljust(6, "\0").bytes[0..5] + encoded_family_name.ljust(6, "\0").bytes[0..5] + [0]).pack("C*")
   end
 
   private
 
   # validation
+
+  def check_first_name_length_be_in_range
+    errors.add(:first_name, ': 6文字以内にしてください') unless self.encoded_first_name.bytesize <= 6
+  end
+
+  def check_family_name_length_be_in_range
+    errors.add(:family_name, ': 6文字以内にしてください') unless self.encoded_family_name.bytesize <= 6
+  end
 
   def check_birth_date_be_past
     errors.add(:birth_date, ': 過去の日付を指定してください') unless self.birth_date <= Date.today
