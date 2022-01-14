b3371f34ea12006987d201a827e2b89b32c6a60f
Crow314 <master@crow31415.net>
2022-01-15 05:37:59 +0900

    Create process of connecting between gateway and interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'shelters#index'
   resources :shelters, only: [:index, :show]
   resources :evacuees, only: [:new, :create]
+  post 'evacuees/register', to: 'evacuees#register', as: :evacuees_register
 end
